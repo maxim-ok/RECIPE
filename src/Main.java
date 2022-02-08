fabca95326c81385278f493533807fdd813dfd88
Maksim Okunev <max.vic.okunev@gmail.com>
2022-02-08 20:53:59 +0300

    Fix

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int eaters = 5; // сколько людей будут есть
+        int eaters = 5; // сколько людей будут  есть
 
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
